1976be9d126f80099d91af6fdbe61353d3a3cdaa
Jerry <jerry@Jerrys-MacBook-Pro.local>
2021-09-18 21:46:36 -0700

    save fav jokes into SharedPreferences

--- app/src/main/java/com/example/dadjoke/MainActivity.java
+++ app/src/main/java/com/example/dadjoke/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import com.example.dadjoke.databinding.ActivityMainBinding;
+import com.google.gson.Gson;
+
 import org.json.JSONException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
     FavJokes favJokes = new FavJokes();
     JokeModel currentJokeModel;
     JokeAdapter adapter;
+    SharedPreferences mPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mPrefs = getSharedPreferences("Jokes", MODE_PRIVATE);
         initUI();
+        loadJokes();
 
         try {
             jokeViewModel.getJoke().observe(this, new Observer<List<JokeModel>>() {
@@ -67,8 +75,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void onAdd(View view) {
         if (!favJokes.getFavJokes().contains(currentJokeModel)) {
-           favJokes.addJoke(currentJokeModel);
+            favJokes.addJoke(currentJokeModel);
+            SharedPreferences.Editor prefsEditor = mPrefs.edit();
+            Gson gson = new Gson();
+            String json = gson.toJson(currentJokeModel);
+            prefsEditor.putString(String.valueOf(System.currentTimeMillis()), json);
+            prefsEditor.commit();
         }
+    }
+
+    public FavJokes loadJokes() {
+
+        Map<String, ?> prefsMap = mPrefs.getAll();
+
+        if (prefsMap.size() == 0) {
+            return null;
+        }
+
+        Gson gson = new Gson();
+
+        for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
+            JokeModel joke = gson.fromJson(entry.getValue().toString(), JokeModel.class);
+            favJokes.addJoke(joke);
+        }
+        return favJokes;
     }
 
     public void onFav(View view) {
